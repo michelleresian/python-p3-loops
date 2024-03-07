58521ca662cd45ee10170f68d0f8fc0b5eda1bfb
michelleresian <michellresian@gmail.com>
2024-03-07 09:39:55 +0300

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while (i >= 1):
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squares_list = [int ** 2 for int in int_list]
+    # print(squares_list)
+    return squares_list
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print ("FizzBuzz")
+        elif number % 3 == 0:
+            print ("Fizz")
+        elif number % 5 == 0:
+            print ("Buzz")
+        else:
+            print (number)
+fizzbuzz()
